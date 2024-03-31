86995cec154c353bd3619b8ee603b59bd8d482c7
Lalelulilulela <e0967877@u.nus.edu>
2024-03-31 14:50:03 +0800

    Update header comments

--- src/main/java/seedu/address/logic/parser/FindTagCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindTagCommandParser.java
@@ -17,8 +17,8 @@ import seedu.address.model.person.TagContainsKeywordsPredicate;
 public class FindTagCommandParser implements Parser<FindTagCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the FindCommand
-     * and returns a FindCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the FindTagCommand
+     * and returns a FindTagCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindTagCommand parse(String args) throws ParseException {
