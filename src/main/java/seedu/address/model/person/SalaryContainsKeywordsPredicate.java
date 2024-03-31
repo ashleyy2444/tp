4afd4d8c2ff8e4ae7a1114de391c0754861d1793
Lalelulilulela <e0967877@u.nus.edu>
2024-03-31 19:39:34 +0800

    Fix checkstyle

--- src/main/java/seedu/address/model/person/SalaryContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/SalaryContainsKeywordsPredicate.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.function.Predicate;
 
 import seedu.address.commons.util.ToStringBuilder;
-import seedu.address.logic.parser.exceptions.ParseException;
 
 
 /**
