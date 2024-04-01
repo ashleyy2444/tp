90871ed414be13a63ea0402706a0e2f47e813d2a
TIANYAO ZHANG <88520881+ZHANGTIANYAO1@users.noreply.github.com>
2024-04-01 21:54:16 +0800

    Update sort command test

--- src/test/java/seedu/address/logic/commands/SortCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortCommandTest.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
@@ -9,6 +10,7 @@ import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -87,7 +89,7 @@ public class SortCommandTest {
     }
 
     @Test
-    public void execute_validJobDifficultyCliUnsortedList_success() {
+    public void execute_validJobDifficultyCliUnsortedList_success() throws CommandException {
         Integer prefixToSort = 5; //sort by Job Difficulty
         SortCommand sortCommand = new SortCommand(prefixToSort);
 
@@ -96,7 +98,9 @@ public class SortCommandTest {
         ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
         expectedModel.updateSortedPersonList(new PersonSalaryComparator());
 
-        assert true;
+        CommandResult result = sortCommand.execute(model);
+
+        assertEquals(result.getFeedbackToUser(), expectedMessage);
     }
 
     @Test
