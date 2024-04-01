fd8c256c9661e0ae04de0fda99bc131a3f40c603
Ashley Chua <ashleycxr2444@gmail.com>
2024-04-02 05:01:18 +0800

    Edit header comments for FindCommand class

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -8,8 +8,8 @@ import seedu.address.model.Model;
 import seedu.address.model.person.NameOrCompanyNameContainsKeywordsPredicate;
 
 /**
- * Finds and lists all persons in address book whose name contains any of the argument keywords.
- * Keyword matching is case insensitive.
+ * Finds and lists all persons in address book whose name or company name contains any of the argument keywords.
+ * Keyword matching is case-insensitive.
  */
 public class FindCommand extends Command {
 
