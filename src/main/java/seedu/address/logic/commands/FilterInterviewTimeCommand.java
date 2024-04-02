ed44509c5c4e7d177f222d2fd8021210e08ad040
Lalelulilulela <e0967877@u.nus.edu>
2024-04-02 23:20:47 +0800

    Add FilterInterviewTimeCommand.java

---
+++ src/main/java/seedu/address/logic/commands/FilterInterviewTimeCommand.java
@@ -0,0 +1,61 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_INTERVIEWTIME;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.person.InterviewTimeContainsKeywordsPredicate;
+
+
+/**
+ * Filters and lists all contacts in address book whose InterviewTime is within the interview time range provided.
+ */
+public class FilterInterviewTimeCommand extends FilterCommand {
+    public static final String COMMAND_WORD = FilterCommand.COMMAND_WORD + " " + PREFIX_INTERVIEWTIME;
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Filters all companies whose "
+            + "range of interview times contain any of "
+            + "the specified salary and displays them as a list with index numbers.\n"
+            + "Parameters: INTERVIEW_TIME_RANGE [MORE_INTERVIEW_TIME_RANGE]...\n"
+            + "Example:\n"
+            + "- " + COMMAND_WORD + "before/020220241100"
+            + "- " + COMMAND_WORD + "after/020220241100"
+            + "- " + COMMAND_WORD + "from/020220241100 to/020520241100";
+
+    private final InterviewTimeContainsKeywordsPredicate predicate;
+
+    public FilterInterviewTimeCommand(InterviewTimeContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredPersonList(predicate);
+        return new CommandResult(
+                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof FilterInterviewTimeCommand)) {
+            return false;
+        }
+
+        FilterInterviewTimeCommand otherFilterInterviewTimeCommand = (FilterInterviewTimeCommand) other;
+        return predicate.equals(otherFilterInterviewTimeCommand.predicate);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("interview time", predicate)
+                .toString();
+    }
+}
